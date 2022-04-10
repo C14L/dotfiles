c36a03ec0bbe6998d6b328f16e3675276635ecc0
Christian Steimel <me@C14L.com>
2022-04-10 23:46:15 +0200

    Delete old backup scripts

--- scripts/backup-home-to-usb-hdd-WD4TB.sh
+++ scripts/backup-home-to-usb-hdd-WD4TB.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SUBDIR="backup-$(hostname)-mount"
+SUBDIR="backup-$(hostname)"
 
 LOGFILE="/tmp/$SUBDIR.log"
 
@@ -20,39 +20,20 @@ echo
 mkdir -p "$TRG"
 
 rsync -avP --delete                           \
-    --exclude="chris/.android/"               \
-    --exclude="chris/.cache/"                 \
-    --exclude="chris/.cargo/"                 \
-    --exclude="chris/.dbus"                   \
-    --exclude="chris/.debug/"                 \
-    --exclude="chris/.gradle/"                \
-    --exclude="chris/.dart/"                  \
-    --exclude="chris/.dartServer/"            \
-    --exclude="chris/.local/share/Trash/"     \
-    --exclude="chris/.local/share/Steam/"     \
-    --exclude="chris/.config/BraveSoftware"   \
-    --exclude="chris/.config/Code"            \
-    --exclude="chris/.npm/"                   \
-    --exclude="chris/.npm-packages/"          \
-    --exclude="chris/.rustup/"                \
-    --exclude="chris/.thumbnails/"            \
-    --exclude="chris/.AndroidStudio*"         \
-    --exclude="chris/.PyCharm*/system/caches" \
-    --exclude="chris/.PyCharm*/system/log"    \
-    --exclude="chris/Android/"                \
-    --exclude="chris/snap/"                   \
-    --exclude="chris/Server/"                 \
-    --exclude="chris/Downloads/"              \
-    --exclude="chris/Movies/"                 \
-    --exclude="chris/**/Cache*"               \
-    --exclude="chris/**/*Cache/"              \
-    --exclude="chris/**/cache*"               \
-    --exclude="chris/**/*cache/"              \
-    --exclude="chris/**/Temp/"                \
-    --exclude="chris/**/temp/"                \
-    --exclude="chris/**/tmp/"                 \
-    --exclude="chris/**/target/debug/"        \
-    --exclude="chris/**/target/rls/"          \
+    --exclude="$USER/.cache/"                 \
+    --exclude="$USER/.dbus"                   \
+    --exclude="$USER/.local/share/Trash/"     \
+    --exclude="$USER/snap/"                   \
+    --exclude="$USER/**/Cache*"               \
+    --exclude="$USER/**/*Cache/"              \
+    --exclude="$USER/**/cache*"               \
+    --exclude="$USER/**/*cache/"              \
+    --exclude="$USER/**/Temp/"                \
+    --exclude="$USER/**/temp/"                \
+    --exclude="$USER/**/tmp/"                 \
+    --exclude="$USER/**/target/debug/"        \
+    --exclude="$USER/**/target/rls/"          \
+    --exclude="$USER/**/*_skipbackup_*"       \
     "$SRC" "$TRG/" > $LOGFILE
 
 touch "$TRG"
