e332ce611d5f3844c60ac552e13699300e67fbca
Christian Steimel <me@C14L.com>
2022-08-07 12:33:02 +0200

    setup: Update Gnome configs

--- setup/install-25-configure-gnome.sh
+++ setup/install-25-configure-gnome.sh
@@ -10,41 +10,49 @@
 #
 ######################################################################
 
-# Disable Ctrl+Shift+Arrow Keys to switch desktop down/up:
+# disable window grouping and enable window switching
 
-# $ gsettings get org.gnome.desktop.wm.keybindings switch-to-workspace-down
-#['<Super>Page_Down', '<Control><Alt>Down']
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Super>Tab']"
 
-gsettings set org.gnome.desktop.wm.keybindings \
-    switch-to-workspace-down "['<Super>Page_Down']"
+# Disable Ctrl+Shift+Arrow Keys to switch desktop down/up, use <Super> instead:
+# Default: ['<Super>Page_Down', '<Control><Alt>Down']
+#          ['<Super>Page_Up', '<Control><Alt>Up']
 
-# $ gsettings get org.gnome.desktop.wm.keybindings switch-to-workspace-up
-#['<Super>Page_Up', '<Control><Alt>Up']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Super>Page_Down']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Super>Page_Up']"
 
-gsettings set org.gnome.desktop.wm.keybindings \
-    switch-to-workspace-up "['<Super>Page_Up']"
+# Move current window between workspaces:
+# Default: ['<Super><Shift>Page_Down', '<Control><Shift><Alt>Down']
+#          ['<Super><Shift>Page_Up', '<Control><Shift><Alt>Up']
 
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['<Super><Shift>Page_Down']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['<Super><Shift>Page_Up']"
 
-# $ gsettings get org.gnome.desktop.wm.keybindings move-to-workspace-down
-#['<Super><Shift>Page_Down', '<Control><Shift><Alt>Down']
+# Unset weird keybiding that messes up multi monitor settings
+# Default: ['<Super>p', 'XF86Display']
 
-gsettings set org.gnome.desktop.wm.keybindings \
-    move-to-workspace-down "['<Super><Shift>Page_Down']"
+gsettings set org.gnome.mutter.keybindings switch-monitor "[]"
 
-# $ gsettings get org.gnome.desktop.wm.keybindings move-to-workspace-up
-#['<Super><Shift>Page_Up', '<Control><Shift><Alt>Up']
+# Unset left/right workspace switching
+# Default: ['<Control><Alt>Left']
+#          ['<Control><Alt>Right']
+#          ['<Control><Shift><Alt>Left']
+#          ['<Control><Shift><Alt>Right']
 
-gsettings set org.gnome.desktop.wm.keybindings \
-    move-to-workspace-up "['<Super><Shift>Page_Up']"
-
-
-# gsettings get org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Control><Alt>Left']"
-# gsettings get org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Control><Alt>Right']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "[]"
-
-# org.gnome.desktop.wm.keybindings move-to-workspace-left ['<Control><Shift><Alt>Left']
-# org.gnome.desktop.wm.keybindings move-to-workspace-right ['<Control><Shift><Alt>Right']
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left []
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right []
+
+# screenshot: ['<Shift>Print']
+# screenshot-window: ['<Alt>Print']
+gsettings set org.gnome.shell.keybindings screenshot "['<Shift><Super>p']"
+gsettings set org.gnome.shell.keybindings screenshot-window "['<Super>p']"
+
+# Nautilus settings
+gsettings set org.gnome.nautilus.preferences show-hidden-files true
+gsettings set org.gnome.nautilus.preferences show-delete-permanently true
 
