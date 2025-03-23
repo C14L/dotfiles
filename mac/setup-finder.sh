d1f65e85329d94cb561f3be3a432d80fc634574f
Christian Steimel <me@C14L.com>
2025-03-23 18:05:25 +0100

    Add mac directory, add script to update Finder settings

---
+++ mac/setup-finder.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+# Some useful settings for MacOS Finder.
+
+# Show hidden files in Finder. GUI: `Cmd + Shift + .`
+defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder
+
+# Show all filename extensions in Finder
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Restart Finder to apply changes
+killall Finder
